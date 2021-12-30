57f0927e47adce381a5f6f9125a60c01687313af
Ian Macalinao <ian@saber.so>
2021-12-30 05:17:25 -0800

    fix download program script

--- scripts/download-programs.sh
+++ scripts/download-programs.sh
@@ -5,5 +5,5 @@ cd $(dirname $0)/..
 mkdir -p artifacts/programs/
 
 # saber
-curl -L https://github.com/saber-hq/stable-swap/releases/download/v1.6.1/stable_swap.so > \
+curl -L https://github.com/saber-hq/stable-swap/releases/download/v1.6.5/stable_swap.so > \
     artifacts/programs/stable_swap.so
